ddb997c8c339406aea5116e5f258f7614b2352a4
GAYA ANALYTICS <jayronsoares@gmail.com>
2023-07-05 23:57:00 -0300

    Add files via upload

---
+++ RestFul API Request Optimization - Cache & Pagination.py
@@ -0,0 +1,158 @@
+"""
+1. Importing necessary libraries:
+   - `requests`: For making API requests.
+   - `pandas`: For working with dataframes.
+   - `numpy`: For array operations and comparisons.
+   - `functools`: For creating a cache decorator.
+   - `time`: For introducing a delay between API requests.
+   - `sqlalchemy`: For connecting to the PostgreSQL data warehouse and loading data into tables.
+
+2. Defining a cache decorator:
+   - The `cache` decorator is used to cache API responses, which helps avoid making redundant API requests for the same data.
+
+3. Making API requests with pagination support:
+   - The `make_api_request` function is decorated with the `cache` decorator to cache API responses.
+   - It makes API requests with pagination to retrieve all data from the API endpoint.
+   - The data is stored in a pandas dataframe and returned.
+
+4. Reading a CSV file:
+   - The `read_csv_file` function reads a CSV file and returns the data as a pandas dataframe.
+
+5. Data cleaning:
+   - The `clean_data` function is responsible for applying data cleaning techniques to the dataframes.
+   - You can implement your own data cleaning techniques specific to your project's requirements.
+
+6. Comparing dataframe schemas:
+   - The `compare_schema` function compares the column names of two dataframes using `np.array_equal` from numpy.
+   - It returns `True` if the schemas are identical.
+
+7. Concatenating dataframes:
+   - The `concatenate_dataframes` function concatenates two dataframes using `pd.concat`.
+   - The resulting concatenated dataframe is returned.
+
+8. Loading data into staging tables:
+   - The `load_into_staging_table` function loads a dataframe into a staging table in the PostgreSQL data warehouse using SQLAlchemy's `create_engine`.
+   - The `if_exists` parameter is set to 'replace' to replace any existing data in the staging table.
+   - Further processing can be performed on the staging table if needed.
+
+9. Loading data into the target table:
+   - The `load_into_target_table` function loads a dataframe into the target table in the PostgreSQL data warehouse using SQLAlchemy's `create_engine`.
+   - The `if_exists` parameter is set to 'replace' to replace any existing data in the target table.
+   - Further processing can be performed on the target table if needed.
+
+10. The `main` function:
+    - The `main` function serves as the entry point for the code.
+    - It defines the API URL, CSV file path, and target table name.
+    - It calls the necessary functions in a sequence to perform the data processing and loading.
+    - If the schemas of the API dataframe and CSV dataframe are identical, the data is concatenated and loaded directly into the target table.
+    - If the schemas are different, the data is loaded into staging tables separately.
+
+Please note that you will need to provide the appropriate database connection parameters (`username`, `password`, and `database`) in the `create_engine` function to establish a connection with your PostgreSQL data warehouse.
+
+Feel free to adapt and customize the code according to your specific requirements and data processing pipeline.
+
+"""
+
+
+import requests
+import pandas as pd
+import numpy as np
+import functools
+import time
+from sqlalchemy import create_engine
+
+# Cache decorator to cache API responses
+def cache(func):
+    cached_data = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key in cached_data:
+            return cached_data[key]
+        else:
+            result = func(*args, **kwargs)
+            cached_data[key] = result
+            return result
+
+    return wrapper
+
+# Make API request with pagination support
+@cache
+def make_api_request(api_url, page_size=100):
+    all_data = []
+    page = 1
+    while True:
+        response = requests.get(f"{api_url}?page={page}&limit={page_size}")
+        api_data = response.json()
+        if not api_data:
+            break
+        all_data.extend(api_data)
+        page += 1
+    api_df = pd.DataFrame(all_data)
+    return api_df
+
+def read_csv_file(csv_file):
+    csv_df = pd.read_csv(csv_file)
+    return csv_df
+
+def clean_data(df):
+    # Apply data cleaning techniques to the dataframe
+    # Replace missing values
+    df.fillna(0, inplace=True)
+    
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+    
+    # Remove outliers
+    df = remove_outliers(df)
+    
+    # Standardize or normalize data
+    df['column1'] = (df['column1'] - df['column1'].mean()) / df['column1'].std()
+    
+    # Perform other data cleaning operations as needed
+    
+    return df
+
+def compare_schema(df1, df2):
+    return np.array_equal(df1.columns, df2.columns)
+
+def concatenate_dataframes(df1, df2):
+    concatenated_df = pd.concat([df1, df2], ignore_index=True)
+    return concatenated_df
+
+def load_into_staging_table(df, table_name):
+    engine = create_engine('postgresql://username:password@localhost:5432/database')
+    df.to_sql(f"staging_{table_name}_table", engine, if_exists='replace', index=False)
+    
+
+def load_into_target_table(df, table_name):
+    engine = create_engine('postgresql://username:password@localhost:5432/database')
+    df.to_sql(table_name, engine, if_exists='replace', index=False)
+    
+
+def main():
+    api_url = 'https://api.example.com/data'
+    csv_file = 'data.csv'
+    target_table_name = 'target_table'
+
+    api_df = make_api_request(api_url)
+    csv_df = read_csv_file(csv_file)
+
+    api_df = clean_data(api_df)
+    csv_df = clean_data(csv_df)
+
+    if compare_schema(api_df, csv_df):
+        concatenated_df = concatenate_dataframes(api_df, csv_df)
+        
+        load_into_target_table(concatenated_df, target_table_name)
+    else:
+        load_into_staging_table(api_df, 'api')
+        load_into_staging_table(csv_df, 'csv')
+        
+        # ...
+
+    return df
+
+if __name__ == '__main__':
+    main()
