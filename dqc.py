ac4f8abb95756413cf9bb61a011583acf8003c10
GAYA ANALYTICS <jayronsoares@gmail.com>
2023-07-20 22:48:28 -0300

    Update dqc.py

--- dqc.py
+++ dqc.py
@@ -1,12 +1,5 @@
-
-"""
-In this code, we define each data quality function and a load_data() function to load the dataset from a CSV file using pandas. 
-We also define the main() function to run each data quality check on the dataset and print out the results to the console. 
-The if __name__ == '__main__': block ensures that the main() function is only executed if the script is run directly, and not if it is imported as a module.
-"""
 import pandas as pd
 import numpy as np
-
 
 def load_data(file_path):
     """
@@ -15,87 +8,93 @@ def load_data(file_path):
     df = pd.read_csv(file_path)
     return df
 
-
 def check_missing_values(df):
     """
     Check for missing values in the dataset using pandas.
     """
-    missing_values = df.isnull().sum()
-    return missing_values
-
+    return df.isnull().sum()
 
 def check_duplicates(df):
     """
     Check for duplicates in the dataset using pandas.
     """
-    duplicates = df.duplicated().sum()
-    return duplicates
-
+    return df.duplicated().sum()
 
 def check_consistency(df, group_columns, check_column):
     """
     Check for consistency in the dataset using pandas.
     """
     group_data = df.groupby(list(group_columns))[check_column].apply(list).reset_index()
-    inconsistent_rows = []
-    for i, row in group_data.iterrows():
-        if len(set(row[check_column])) > 1:
-            inconsistent_rows.append(row)
-    return len(inconsistent_rows)
+    return group_data.apply(lambda row: len(set(row[check_column])) > 1, axis=1).sum()
 
-
-def check_accuracy(df, check_column, min_value, max_value):
+def check_accuracy_validity(df, column):
     """
-    Check for accuracy in the dataset using numpy.
+    Check for accuracy and validity in the dataset using numpy and pandas.
     """
-    accuracy_count = np.logical_and(df[check_column] >= min_value, df[check_column] <= max_value).sum()
+    min_value, max_value = df[column].min(), df[column].max()
+    valid_values = df[column].unique()
+    
+    accuracy_count = np.logical_and(df[column] >= min_value, df[column] <= max_value).sum()
     accuracy_percent = accuracy_count / len(df) * 100
-    return accuracy_count, accuracy_percent
-
-
-def check_validity(df, check_column, valid_values):
-    """
-    Check for validity in the dataset using pandas.
-    """
-    validity_count = df[check_column].isin(valid_values).sum()
+    
+    validity_count = df[column].isin(valid_values).sum()
     validity_percent = validity_count / len(df) * 100
-    return validity_count, validity_percent
+    
+    return accuracy_count, accuracy_percent, validity_count, validity_percent
 
+def check_cardinality(df, column):
+    """
+    Check if a column has high or low cardinality.
+    """
+    cardinality = df[column].nunique()
+    high_cardinality = cardinality > len(df) / 2
+    low_cardinality = cardinality < 0.01 * len(df)
+    return high_cardinality, low_cardinality
+
+def print_data_quality_results(results_dict):
+    """
+    Print data quality results in a formatted way.
+    """
+    for column, result in results_dict.items():
+        print(f"Column: {column}")
+        print("Missing Values:", result["Missing Values"])
+        print("Duplicates:", result["Duplicates"])
+        print("High Cardinality:", result["High Cardinality"])
+        print("Low Cardinality:", result["Low Cardinality"])
+        print("Consistency Issues:", result["Consistency Issues"])
+        print("Accuracy Count:", result["Accuracy Count"])
+        print("Accuracy Percentage:", result["Accuracy Percentage"])
+        print("Validity Count:", result["Validity Count"])
+        print("Validity Percentage:", result["Validity Percentage"])
+        print("\n")
 
 def main():
     # Load the dataset
-    file_path = 'data.csv'
+    file_path = "Suicide data.csv"
     df = load_data(file_path)
 
-    # Check for missing values
-    missing_values = check_missing_values(df)
-    print("Missing Values:\n", missing_values)
+    # Dictionary to store data quality results
+    results_dict = {}
 
-    # Check for duplicates
-    duplicates = check_duplicates(df)
-    print("Number of Duplicates:", duplicates)
+    # Check for data quality for each column in the DataFrame
+    for column in df.columns:
+        results_dict[column] = {
+            "Missing Values": check_missing_values(df[column]),
+            "Duplicates": check_duplicates(df[column]),
+            "High Cardinality": check_cardinality(df, column)[0],
+            "Low Cardinality": check_cardinality(df, column)[1],
+            "Consistency Issues": check_consistency(df, group_columns=df.drop(column, axis=1).columns[:2], check_column=column),
+        }
+        
+        # Get accuracy and validity results
+        accuracy_count, accuracy_percent, validity_count, validity_percent = check_accuracy_validity(df, column)
+        results_dict[column]["Accuracy Count"] = accuracy_count
+        results_dict[column]["Accuracy Percentage"] = accuracy_percent
+        results_dict[column]["Validity Count"] = validity_count
+        results_dict[column]["Validity Percentage"] = validity_percent
 
-    # Check for consistency
-    group_columns = ('column1', 'column2')
-    check_column = 'column3'
-    consistency = check_consistency(df, group_columns, check_column)
-    print("Number of Inconsistent Rows:", consistency)
-
-    # Check for accuracy
-    check_column = 'column4'
-    min_value = 0.0
-    max_value = 1.0
-    accuracy_count, accuracy_percent = check_accuracy(df, check_column, min_value, max_value)
-    print("Number of Accurate Rows:", accuracy_count)
-    print("Accuracy Percentage:", accuracy_percent)
-
-    # Check for validity
-    check_column = 'column5'
-    valid_values = ('value1', 'value2', 'value3')
-    validity_count, validity_percent = check_validity(df, check_column, valid_values)
-    print("Number of Valid Rows:", validity_count)
-    print("Validity Percentage:", validity_percent)
-
+    # Print data quality results
+    print_data_quality_results(results_dict)
 
 if __name__ == '__main__':
     main()
